4a8d9338a48606d0f0940ef09f8015e1d7c07164
memetibryan <memetibryan@gmail.com>
2017-07-20 12:50:35 +0300

    added templated and template rendering using the Apache Velocity Engine and intedrated it using an adapter to spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,45 +1,20 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
   public static void main(String[] args) {
     staticFileLocation("/public");
-    get("/", (request, response) ->
-       "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-          "<title>Hello Friend!</title>" +
-          "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-        "</head>" +
-       "<body>" +
-          "<h1>Hello From Afar</h1>" +
-          "<p>Dear Friend,</p>" +
-          "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-          "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-          "<p>But I like programming a lot, so I've got that going for me. </p>" +
-          "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-          "<p>Cheers,</p>" +
-          "<p>Travel Enthusiast Jane</p>" +
-          "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
-        "</body>" +
-      "</html>"
-   );
 
-   get("/favorite_photos", (request, response) ->
-    "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Hello Friend!</title>" +
-        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-       "<h1>Favorite Traveling Photos</h1>" +
-          "<ul>" +
-            "<li><img src='/images/bryan.jpg' alt='A photo of a mountain.'/></li>" +
-            "<li><img src='/images/memeti.jpg' alt='A photo of a a rocky beach.'/></li>" +
-          "</ul>" +
-      "</body>" +
-      "</html>"
-    );
+    get("/", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/hello.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/favorite_photos", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+    }, new VelocityTemplateEngine());
 
   }
 }
